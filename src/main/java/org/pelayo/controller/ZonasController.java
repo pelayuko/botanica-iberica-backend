3d80910f59c8707fe56849efcd08d7090b01eec4
Javier Pelayo <javierpelayozueco@gmail.com>
2015-05-02 15:28:50 +0200

    sectores en BBDD

--- src/main/java/org/pelayo/controller/ZonasController.java
+++ src/main/java/org/pelayo/controller/ZonasController.java
@@ -11,6 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ZonasController {
 
+	// FIXME: do it with query -- Arreglado
+/*	static public String[][] SECTORES = { { "A", "La Muela" }, { "B", "Campo de Borja" }, { "C", "La Sierra" },
+			{ "D", "El Moncayo" }, { "E", "Somontano" }, { "F", "Magallón" }, { "G", "Tarazona" }, { "H", "Ágreda" },
+			{ "Z", "Otras" } };
+
+	public static String nombreSectorFromEtiq(String etiq){
+		int temp = (int) (etiq.charAt(0)) - 64;
+		if (temp < 10)
+			return SECTORES[temp - 1][1];
+		else return "Desconocida";
+	}*/
+	
 	@Autowired
 	private TaxonesRepository repo;
 
