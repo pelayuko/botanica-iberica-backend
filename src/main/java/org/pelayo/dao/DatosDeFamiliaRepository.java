d517904a3bfb4016eb2bc897cba3fb3871f345a0
pelayuko <jpelayo2008@gmail.com>
2015-05-03 21:49:29 +0200

    foto para listado de familia

--- src/main/java/org/pelayo/dao/DatosDeFamiliaRepository.java
+++ src/main/java/org/pelayo/dao/DatosDeFamiliaRepository.java
@@ -19,14 +19,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional(readOnly = true)
 public class DatosDeFamiliaRepository {
-	
+
 	private static final Logger log = Logger.getLogger(DatosDeFamiliaRepository.class);
 
 	// FIXME: simplify, hibernate, reuse, entities not views...
 
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
+	@Autowired
+	DatosDeEspecieRepository datosDeEspecieRepository;
+
 	public List<TaxonResponse> getTaxonesByFamilia(String familia) {
 		String query = "select identEsp, Género, elNombre, Familia from ConsEspecie where Familia = '" + familia + "'";
 		return jdbcTemplate.query(query,
@@ -34,7 +37,9 @@ public class DatosDeFamiliaRepository {
 		new RowMapper<TaxonResponse>() {
 			@Override
 			public TaxonResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new TaxonResponse(rs.getString("elNombre"), "", rs.getString("Género"), rs.getString("Familia"));
+				String nombre = rs.getString("elNombre");
+				return new TaxonResponse(nombre, "", rs.getString("Género"), rs.getString("Familia"),
+						datosDeEspecieRepository.getRandomFoto(nombre));
 			}
 		});
 	}
@@ -42,40 +47,40 @@ public class DatosDeFamiliaRepository {
 	public List<FotoResponse> getListFotos(String familia, int limit) {
 		String consulta = "select flickrUrl, elNombre, ifnull(comentario,'sin coment.') as coment, ifnull(UTM,'-') as UTM from consfotos where Familia = '"
 				+ familia + "' order by rand() limit " + limit;
-		List<FotoResponse> results = jdbcTemplate.query(consulta, 
-				new RowMapper<FotoResponse>() {
-					@Override
-					public FotoResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
-						String temp = rs.getString("flickrUrl");
-						return new FotoResponse(temp, rs.getString("coment"), rs.getString("UTM"), rs.getString("elNombre"));
-					}
-				});
+		List<FotoResponse> results = jdbcTemplate.query(consulta, new RowMapper<FotoResponse>() {
+			@Override
+			public FotoResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+				String temp = rs.getString("flickrUrl");
+				return new FotoResponse(temp, rs.getString("coment"), rs.getString("UTM"), rs.getString("elNombre"));
+			}
+		});
 		return results;
 
 	}
 
-    public String getRefFlora(String laFamilia) {		
+	public String getRefFlora(String laFamilia) {
 		String flora = jdbcTemplate.queryForObject(
-				"select ifnull(refFloraIberica,'-') as refFlIb from Familias where NombreFam = '" + laFamilia + "'", new RowMapper<String>() {
+				"select ifnull(refFloraIberica,'-') as refFlIb from Familias where NombreFam = '" + laFamilia + "'",
+				new RowMapper<String>() {
 					@Override
-					public String mapRow(ResultSet rs, int rowNum)
-							throws SQLException {
+					public String mapRow(ResultSet rs, int rowNum) throws SQLException {
 						return rs.getString("refFlIb");
 					}
 				});
-		if (flora.equals("-")) return "http://www.floraiberica.es";
-		else return "http://www.floraiberica.es/floraiberica/texto/pdfs/" + flora +".pdf";
-    }
-    
-    public String getGrupoFam(String laFamilia) {
-		String grupo = jdbcTemplate.queryForObject(
-				"select grupoFam from Familias where NombreFam = '" + laFamilia + "'", new RowMapper<String>() {
-					@Override
-					public String mapRow(ResultSet rs, int rowNum)
-							throws SQLException {
-						return rs.getString("grupoFam");
-					}
-				});
+		if (flora.equals("-"))
+			return "http://www.floraiberica.es";
+		else
+			return "http://www.floraiberica.es/floraiberica/texto/pdfs/" + flora + ".pdf";
+	}
+
+	public String getGrupoFam(String laFamilia) {
+		String grupo = jdbcTemplate.queryForObject("select grupoFam from Familias where NombreFam = '" + laFamilia
+				+ "'", new RowMapper<String>() {
+			@Override
+			public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return rs.getString("grupoFam");
+			}
+		});
 		return grupo;
-    }
+	}
 }
