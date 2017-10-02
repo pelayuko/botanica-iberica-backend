a52889b48408495f7ab2b690ed70c43cc1cfcea1
Pelayo, Javier <Javier.Pelayo@adidas-group.com>
2017-10-02 07:47:13 +0200

    mongo disabled

--- src/main/java/org/pelayo/BotanicaBackendApplication.java
+++ src/main/java/org/pelayo/BotanicaBackendApplication.java
@@ -7,13 +7,14 @@ import org.pelayo.util.QuotaGuardProxyAuthenticator;
 import org.pelayo.util.QuotaGuardProxySelector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class BotanicaBackendApplication {
 
 	public static void main(String[] args) {
