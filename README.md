e36569fef5c859a2ba2d37fd1dfed790a49bce30
Pelayo, Javier <Javier.Pelayo@adidas-group.com>
2017-10-07 09:51:30 +0200

    readme modified

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # botanica-iberica-backend
 Backend for botanica iberica project
 
-How to deploy in Openshift
-
-oc new-app codecentric/springboot-maven3-centos~https://github.com/pelayuko/botanica-iberica-backend.git
--- src/main/java/org/pelayo/filter/SimpleCORSFilter.java
+++ src/main/java/org/pelayo/filter/SimpleCORSFilter.java
@@ -21,11 +21,11 @@ public class SimpleCORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
 			ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", allowOrigin);
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		HttpServletResponse test = (HttpServletResponse) res;
+		test.setHeader("Access-Control-Allow-Origin", allowOrigin);
+		test.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		test.setHeader("Access-Control-Max-Age", "3600");
+		test.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		chain.doFilter(req, res);
 	}
 
